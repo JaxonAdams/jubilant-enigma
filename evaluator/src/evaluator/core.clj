0b7cb725fefd7e61c2582e2bfb1a1fecf2188104
Jaxon Adams <jadamsresume1@gmail.com>
2023-03-01 19:13:11 -0700

    very barebones cli clj evaluator

--- evaluator/src/evaluator/core.clj
+++ evaluator/src/evaluator/core.clj
@@ -1,7 +1,27 @@
 (ns evaluator.core
   (:gen-class))
 
+
+
+(defn collect-input
+  [inputs]
+  (def new-input (read-line))
+  (if (empty? new-input)
+    (apply str inputs)
+    (collect-input (conj inputs new-input))))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Please enter a valid clojure expression...")
+  ;; (def user-input (read-line))
+  (def user-input (collect-input []))
+  (println user-input)
+  (def expr (read-string user-input))
+  (println "\nYour expression:")
+  (println expr)
+  (println "\nEvaluated:")
+  (println (eval expr)))
+
+
